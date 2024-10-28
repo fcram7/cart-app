c1b9b0727b217d53304e2be4a8d5cc8ae8e4892d
fachri achmad maulana <fachri57@yahoo.com>
2024-10-28 16:36:36 +0700

    added products section

---
+++ src/app/Products.tsx
@@ -0,0 +1,8 @@
+export const Products = () => {
+  return (
+    <section className='products-section px-[4%] xl:px-[1%] py-16 lg:py-20'>
+      <h1 className='products-section__section-title lg:text-4xl font-semibold'>Products</h1>
+      <div className='products-section__content'></div>
+    </section>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from './Hero';
+import { Products } from './Products';
 
 export default function Home() {
   return (
     <main className='font-[family-name:var(--font-geist-sans)]'>
       <Hero />
+      <Products />
     </main>
   );
 }
