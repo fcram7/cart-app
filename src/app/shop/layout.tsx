34dbfd0d15dca6ab5c5353a6ce1aa8e615f42f90
fachri achmad maulana <fachri57@yahoo.com>
2024-11-01 20:24:17 +0700

    added metadata for shop page

--- src/app/shop/layout.tsx
+++ src/app/shop/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Cart | Shop',
+  description: 'Cart shop page'
+}
 
 export default function ShopLayout({
   children
